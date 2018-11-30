15afb89d48fdf272afada0a762908fdae0f0ad8f
Unknown <elcentaleon@gmail.com>
2018-11-30 17:12:07 +0100

    30/11/2018 (house)

--- c#/DB/dbC/WindowsFormsApp1/Form1.Designer.cs
+++ c#/DB/dbC/WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.listEMP = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buscador : ";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(207, 21);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(256, 34);
+            this.txtApellido.TabIndex = 1;
+            this.txtApellido.TextChanged += new System.EventHandler(this.txtApellido_TextChanged);
+            // 
+            // listEMP
+            // 
+            this.listEMP.FormattingEnabled = true;
+            this.listEMP.ItemHeight = 27;
+            this.listEMP.Location = new System.Drawing.Point(17, 70);
+            this.listEMP.Name = "listEMP";
+            this.listEMP.Size = new System.Drawing.Size(512, 382);
+            this.listEMP.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(541, 456);
+            this.Controls.Add(this.listEMP);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.ListBox listEMP;
     }
 }
 
--- c#/DB/dbC/WindowsFormsApp1/Form1.cs
+++ c#/DB/dbC/WindowsFormsApp1/Form1.cs
@@ -12,9 +12,49 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        String strCon;
+        SqlConnection con;
+        SqlCommand com;
+        SqlDataReader reader;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void StartConnection()
+        {
+            this.strCon = "Data Source = LOCALHOST;" +
+                " Initial Catalog = HOSPITAL;" +
+                " Persist Security Info = True;" +
+                "User ID = SA;" +
+                "Password = 'MCSD2018'";
+            this.con = new SqlConnection(this.strCon);
+            this.com = new SqlCommand();
+            this.com.Connection = this.con;
+            this.com.CommandType = CommandType.Text;
+            ShowEMP();
+        }
+        private void ShowEMP()
+        {
+            this.listEMP.Items.Clear();
+            this.com.CommandText = "SELECT APELLIDO,OFICIO FROM EMP " +
+                "WHERE APELIDO LIKE '%' " +
+                this.txtApellido.Text +
+                "'%'";
+            this.con.Open();
+            this.reader = this.com.ExecuteReader();
+            while (this.reader.Read())
+            {
+                this.listEMP.Items.Add(this.reader["APELLIDO"] + "----" + this.reader["OFICIO"]);
+            }
+            this.reader.Close();
+            this.con.Close();
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+            ShowEMP();
         }
     }
 }
