13e6424d40ac2e28ebcdcc7e5501d641882de131
Unknown <elcentaleon@gmail.com>
2018-12-20 18:42:06 +0100

    HOME

--- TCAP/TCAP/Controllers/AccountController.cs
+++ TCAP/TCAP/Controllers/AccountController.cs
@@ -15,10 +15,10 @@ namespace TCAP.Controllers
         // GET: Login
         public ActionResult Login()
         {
-            if (!(TempData["Success"] is null))
-            {
-                ViewBag.Success = TempData["Success"].ToString();
-            }
+            //if (!(TempData["Success"] is null))
+            //{
+            //    ViewBag.Success = TempData["Success"].ToString();
+            //}
 
             return View();
         }
@@ -26,22 +26,23 @@ namespace TCAP.Controllers
         [HttpPost]
         public ActionResult Login(String email, String password)
         {
-            User u = h.Login(email, password);
-            if (!(u.Email is null) && u.Rol == 1)
-            {
-                TempData["obj"] = u;
-                return RedirectToAction("Home", "Client");
-            }
-            else if (!(u.Email is null) && u.Rol == 2)
-            {
-                TempData["obj"] = u;
-                return RedirectToAction("Home", "Player");
-            }
-            else
-            {
-                ViewBag.Error = "El email o la contraseña son incorrectos.";
-                return View();
-            }
+            //User u = h.Login(email, password);
+            //if (!(u.Email is null) && u.Rol == 1)
+            //{
+            //    TempData["obj"] = u;
+            //    return RedirectToAction("Home", "Client");
+            //}
+            //else if (!(u.Email is null) && u.Rol == 2)
+            //{
+            //    TempData["obj"] = u;
+            //    return RedirectToAction("Home", "Player");
+            //}
+            //else
+            //{
+            //    ViewBag.Error = "El email o la contraseña son incorrectos.";
+            //    return View();
+            //}
+            return View();
         }
         // GET: Register
         public ActionResult Register()
@@ -53,16 +54,17 @@ namespace TCAP.Controllers
         public ActionResult Register(String user, String name, String surname, String email, String password,
             String country, String cp_zip)
         {
-            if (h.Register(user,name,surname,email,password,country,cp_zip))
-            {
-                TempData["Success"] = "Se ha registrado correctamente.";
-                return RedirectToAction("Login","Account");
-            }
-            else
-            {
-                ViewBag.Error = "Se ha Producido un error en el registro... Intentelo mas tarde.";
-                return View();
-            }
+            //if (h.Register(user,name,surname,email,password,country,cp_zip))
+            //{
+            //    TempData["Success"] = "Se ha registrado correctamente.";
+            //    return RedirectToAction("Login","Account");
+            //}
+            //else
+            //{
+            //    ViewBag.Error = "Se ha Producido un error en el registro... Intentelo mas tarde.";
+            //    return View();
+            //}
+            return View();
         }
         //GET: PlayerConfirm
         public ActionResult PlayerConfirm(String token = null)
--- TCAP/TCAP/Models/Helpers/HelperAccount.cs
+++ TCAP/TCAP/Models/Helpers/HelperAccount.cs
@@ -1,28 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Configuration;
+using TCAP.Models;
 
 namespace TCAP.Models.Helpers
 {
     public class HelperAccount
     {
-        SqlConnection con;
-        SqlCommand com;
-        SqlDataReader reader;
-        TCAPContextDataContext c;
+
+       
         //CAMBIAR TODO A LINQ Y SIN USAR PROCEDURES
         //REALIZAR AQUÍ LA VALIDACIÓN
         public HelperAccount()
         {
-            String connect = ConfigurationManager.ConnectionStrings["HomeConnection"].ConnectionString;
-            this.con = new SqlConnection(connect);
-            this.com = new SqlCommand();
-            this.com.Connection = this.con;
-            this.com.CommandType = System.Data.CommandType.Text;
-            c = new TCAPContextDataContext();
+
+           
         }
         private String CreateToken()
         {
@@ -49,129 +43,47 @@ namespace TCAP.Models.Helpers
             }
             return true;
         }
-        public Boolean Register(String user, String name, String surname, String email, String password,
-           String country, String cp_zip)
+        private Boolean ExistsEmail(String email)
         {
-            String sql = "REGISTER";
-            SqlParameter r = new SqlParameter("@ROL", user);
-            this.com.Parameters.Add(r);
-            SqlParameter n = new SqlParameter("@N", name);
-            this.com.Parameters.Add(n);
-            SqlParameter s = new SqlParameter("@S", surname);
-            this.com.Parameters.Add(s);
-            SqlParameter e = new SqlParameter("@E", email);
-            this.com.Parameters.Add(e);
-            SqlParameter p = new SqlParameter("@P", Hashing.HashPassword(password));
-            this.com.Parameters.Add(p);
-            SqlParameter c = new SqlParameter("@C", country);
-            this.com.Parameters.Add(c);
-            SqlParameter cp = new SqlParameter("@CP", cp_zip);
-            this.com.Parameters.Add(cp);
+            int? linq = (from d in c.USERS
+                       where d.EMAIL_USER == email
+                       select d.ID_USER).FirtOrDefault();
 
-            this.com.Connection = this.con;
-            this.com.CommandText = sql;
-            this.com.CommandType = System.Data.CommandType.StoredProcedure;
-
-            this.con.Open();
-
-            int afec = this.com.ExecuteNonQuery();
-
-            this.com.Parameters.Clear();
-            this.con.Close();
-
-            if (afec > 0)
+            if (linq is null)
             {
-                Token(email, CreateToken());
+                return true;
+            }
+            return false;
+            
+        }
+        private void ValidateToken(String token) { }
+        public Boolean Register(String user, String name, String surname, String email, String password)
+        {
+            if (ExistsEmail(email))
+            {
+                USERS u = new USERS();
+                u.ID_ROL = user;
+                u.NAME_USER = name;
+                u.SURNAME_USER = surname;
+                u.EMAIL_USER = email;
+                u.PASSWORD_USER = Hashing.HashPassword(password);
+                //insertar usuario
+
+                TOKENS t = new TOKENS();
+                t.ID_USER = u.ID_USER;
+                t.VALUE_TOKEN = CreateToken();
+                t.START_TOKEN = new DateTime();
+                t.END_TOKEN = new DateTime(t.START_TOKEN).AddMinutes(15);
+                //insertar token
+
                 return true;
             }
 
             return false;
         }
-        public User Login(String email, String password)
+        public void  Login(String email, String password)
         {
-            String sql = "LOGON";
-            SqlParameter e = new SqlParameter("@E", email);
-            this.com.Parameters.Add(e);
-            SqlParameter p = new SqlParameter("@P",password);
-            this.com.Parameters.Add(p);
-            this.com.Connection = this.con;
-            this.com.CommandText = sql;
-            this.com.CommandType = System.Data.CommandType.StoredProcedure;
-            this.con.Open();
-
-            this.reader = this.com.ExecuteReader();
-            this.reader.Read();
-            User result = new User();
-
-            if (this.reader.HasRows)
-            {
-                if (Hashing.ValidatePassword(password, this.reader["PASSWORD_USER"].ToString()))
-                {
-                    result.Rol = int.Parse(this.reader["ID_ROL"].ToString());
-                    result.Name = this.reader["NAME_USER"].ToString();
-                    result.Surname = this.reader["SURNAME_USER"].ToString();
-                    result.Email = this.reader["EMAIL_USER"].ToString();
-                }
-            }
-
-            this.com.Parameters.Clear();
-            this.reader.Close();
-            this.con.Close();
-
-            return result;
-
         }        
-        public void Token(String email,String token)
-        {
-            String sql = "TOKEN";
-            SqlParameter e = new SqlParameter("@E", email);
-            this.com.Parameters.Add(e);
-            SqlParameter t = new SqlParameter("@T", token);
-            this.com.Parameters.Add(t);
-            this.com.Connection = this.con;
-            this.com.CommandText = sql;
-            this.com.CommandType = System.Data.CommandType.StoredProcedure;
-
-            this.con.Open();
-            int afect = this.com.ExecuteNonQuery();
-
-            this.com.Parameters.Clear();
-            this.con.Close();
-
-        }
-        public List<Object> ValidateToken(String token)
-        {
-            //String sql = "CONFIRM";
-            //SqlParameter e = new SqlParameter("@T", token);
-            //this.com.Parameters.Add(e);
-            //this.com.Connection = this.con;
-            //this.com.CommandText = sql;
-            //this.com.CommandType = System.Data.CommandType.StoredProcedure;
-            //this.con.Open();
-
-            //this.reader = this.com.ExecuteReader();
-            //this.reader.Read();
-            //User result = new User();
-
-            //if (this.reader.HasRows)
-            //{
-            //    result.Rol = int.Parse(this.reader["ID_ROL"].ToString());
-            //    result.Name = this.reader["NAME_USER"].ToString();
-            //    result.Surname = this.reader["SURNAME_USER"].ToString();
-            //    result.UserId = int.Parse(this.reader["ID_USER"].ToString());  
-            //}
-
-            //this.com.Parameters.Clear();
-            //this.reader.Close();
-            //this.con.Close();
-
-            //return result;
-            List<Object> result = new List<Object>();
-            //String error = null;
-            //CONFIRMResult linq = c.CONFIRM(token, ref error).FirstOrDefault();
-            //result.Add(linq);
-            //result.Add(error);
-            return result;
-        }
+      
     }
 }
