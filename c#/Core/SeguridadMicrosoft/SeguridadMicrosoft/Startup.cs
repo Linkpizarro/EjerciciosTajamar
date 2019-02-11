6b0f2d447641912fffc461056e8fd0e80872ddf8
Unknown <elcentaleon@gmail.com>
2019-02-11 13:31:35 +0100

    others

---
+++ c#/Core/SeguridadMicrosoft/SeguridadMicrosoft/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SeguridadMicrosoft.Data;
+
+namespace SeguridadMicrosoft
+{
+    public class Startup
+    {
+        IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>
+                (options => options.UseSqlServer(configuration["Hospital:ConnectionString"]));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //CONFIGURAR LA SEGURIDAD DEL PROVEEDOR
+            services.AddAuthentication().AddMicrosoftAccount(
+                options => {
+                    options.ClientId = configuration["Authentication:Microsoft:ApplicationId"];
+                    options.ClientSecret = configuration["Authentication:Microsoft:Password"];
+                });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            //6. seguridad middleware
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
