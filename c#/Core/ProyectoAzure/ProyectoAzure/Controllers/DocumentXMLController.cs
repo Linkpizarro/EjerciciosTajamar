5229230be1a07ad249e9333845bd1ed0e67d11ca
Unknown <elcentaleon@gmail.com>
2019-02-15 12:55:35 +0100

    1.ProyectoAzure

---
+++ c#/Core/ProyectoAzure/ProyectoAzure/Controllers/DocumentXMLController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoAzure.Models;
+using ProyectoAzure.Providers;
+
+namespace ProyectoAzure.Controllers
+{
+    public class DocumentXMLController : Controller
+    {
+        PathProvider pathprivider;
+        public DocumentXMLController(PathProvider pathprivider)
+        {
+            this.pathprivider = pathprivider;
+        }
+
+        public IActionResult Joyerias()
+        {
+            String filename = "joyeria.xml";
+            XDocument docxml = XDocument.Load(pathprivider.MapPath(filename,Folders.Documents));
+
+            var joyerias = from data in docxml.Descendants("joyeria")
+                           select new Joyeria
+                           {
+                               Nombre = data.Element("nombrejoyeria").Value,
+                               Direccion = data.Element("direccion").Value,
+                               Telefono = data.Element("telf").Value
+                           };
+
+
+            return View(joyerias);
+        }
+    }
+}
---
+++ c#/Core/ProyectoAzure/ProyectoAzure/Models/Joyeria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoAzure.Models
+{
+    public class Joyeria
+    {
+        public String Nombre { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+    }
+}
