6d78bf9a35533cd49842e7b443d595d92ac465a7
Unknown <elcentaleon@gmail.com>
2019-02-25 10:50:41 +0100

    NADA

---
+++ c#/MVC/AzureMVC/AzureMVC/Controllers/CatastroController.cs
@@ -0,0 +1,37 @@
+﻿using AzureMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace AzureMVC.Controllers
+{
+    public class CatastroController : Controller
+    {
+        ServicioCatastro.CallejerodelasedeelectrónicadelcatastroSoapClient cliente;
+        public CatastroController()
+        {
+            this.cliente = new ServicioCatastro.CallejerodelasedeelectrónicadelcatastroSoapClient();
+        }
+        // GET: Catastro
+        public ActionResult Provincias()
+        {
+            XmlNode nodo = cliente.ObtenerProvincias();
+            String datos = nodo.OuterXml;
+            XDocument doc = XDocument.Parse(datos);
+            XNamespace ns = "http://www.catastro.meh.es/";
+            var consulta = from data in doc.Descendants(ns + "prov")
+                           select new Provincia
+                           {
+                               Codigo = int.Parse(data.Element(ns + "cpine").Value),
+                               Nombre = data.Element(ns + "np").Value
+                           };
+            List<Provincia> provincias = consulta.ToList();
+
+            return View(provincias);
+        }
+    }
+}
---
+++ c#/MVC/AzureMVC/AzureMVC/Models/Provincia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureMVC.Models
+{
+    public class Provincia
+    {
+        public int Codigo { get; set; }
+        public String Nombre { get; set; }
+    }
+}
